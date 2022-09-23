8fee4f1e766f759c96f835ebe12b9e99e9b719d5
谢炉琪 <1103404090@qq.com>
2022-09-23 15:17:35 +0800

    fix 修改发布流程

--- README.md
+++ README.md
@@ -13,9 +13,9 @@ $ npm install                      # 1.安装依赖
 
 $ npm run build                    # 2.打包构建（rollup）,生成dist目录,即打包后文件目录
 
-$ npm run publish -- -v <版本号>    # 3.打包发布
+$ npm run push -- -v <版本号>    # 3.打包发布
 
-例如：npm run publish -- -v 1.0.2
+例如：npm run push -- -v 1.0.2
 
 
 创建一个新的函数，根据提示填写函数名和描述
--- scripts/publish.js
+++ scripts/publish.js
@@ -2,40 +2,49 @@
  * @Author: 谢炉琪
  * @Date: 2022-09-20 14:56:54
  * @LastEditors: 谢炉琪
- * @LastEditTime: 2022-09-20 14:59:50
+ * @LastEditTime: 2022-09-23 15:16:04
  * @Description: 修改包名，获取正确的版本号并修改
  */
 
+const fs = require('fs');
 const path = require('path');
 const shelljs = require('shelljs');
 const program = require('commander');
 
-const targetFile = path.resolve(__dirname, '../dist/package.json');
-const packagejson = require(targetFile);
-const currentVersion = packagejson.version;
-const versionArr = currentVersion.split('.');
-const [mainVersion, subVersion, phaseVersion] = versionArr;
 
-// 默认版本号
-const defaultVersion = `${mainVersion}.${subVersion}.${+phaseVersion+1}`;
+const fnExiting = fs.readdirSync(path.join(__dirname, '../dist'));
+if (fnExiting) {
+  const targetFile = path.resolve(__dirname, '../dist/package.json');
+  const packagejson = require(targetFile);
+  const currentVersion = packagejson.version;
+  const versionArr = currentVersion.split('.');
+  const [mainVersion, subVersion, phaseVersion] = versionArr;
 
-let newVersion = defaultVersion;
+  // 默认版本号
+  const defaultVersion = `${mainVersion}.${subVersion}.${+phaseVersion+1}`;
 
-// 从命令行参数中取版本号
-program
-  .option('-v, --versions <type>', 'Add release version number', defaultVersion);
+  let newVersion = defaultVersion;
 
-program.parse(process.argv);
 
-if (program.versions) {
-  newVersion = program.versions;
+  // 从命令行参数中取版本号
+  program
+    .option('-v, --versions <type>', 'Add release version number', defaultVersion);
+
+  program.parse(process.argv);
+
+
+  const options = program.opts();
+
+  if (options.versions) {
+    newVersion = options.versions;
+  }
+
+  function publish() {
+    shelljs.sed('-i', '"name": "ktools"', '"name": "@kagol/ktools"', targetFile); // 修改包名
+    shelljs.sed('-i', `"version": "${currentVersion}"`, `"version": "${newVersion}"`, targetFile); // 修改版本号
+    shelljs.cd('dist');
+    shelljs.exec('npm publish --access public'); // 发布
+  }
+
+  publish();
 }
-
-function publish() {
-  shelljs.sed('-i', '"name": "ktools"', '"name": "@kagol/ktools"', targetFile); // 修改包名
-  shelljs.sed('-i', `"version": "${currentVersion}"`, `"version": "${newVersion}"`, targetFile); // 修改版本号
-  shelljs.cd('dist');
-  shelljs.exec('npm publish'); // 发布
-}
-
-publish();
